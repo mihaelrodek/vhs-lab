6d1f9ea754d0abc42189f700bf14994a05f88fd3
mrodek <mihael.rodek@true-north.hr>
2021-10-25 13:25:32 +0200

    Project initialized with spring initalizr

---
+++ src/test/java/com/truenorth/vhslab/VhsLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.truenorth.vhslab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VhsLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
